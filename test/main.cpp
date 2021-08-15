9f3e8f35119f974bdc7b6be301875278aa1ca24f
simon <simoncornaz@gmail.com>
2021-08-15 12:50:57 +0200

    try more direct approach

--- test/main.cpp
+++ test/main.cpp
@@ -20,19 +20,13 @@ void cleanup() { WSACleanup(); }
 int wmain(int argc, wchar_t** wargv) {
     initialize_winsock();
     auto server_thread = server();
-    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-    // for(;;) {}
     auto client = lang_main();
-    // auto client = static_cast<SC_TerminalClient*>(createLanguageClient("test"));
-    // auto _ = std::thread([&]() { client->run(0, nullptr); });
     std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-    client->msg("SynthDescLib.global.synthDescs.at(\\default)");
-    client->msg("a = s.options;"
-                "t = Server.remote(\"bob\", NetAddr(\"127.0.0.1\",57110), a, 0);");
+    client->msg("Server.default = Server.remote('test', NetAddr(\"127.0.0.1\",57110), ServerOptions.new, 0);");
     std::this_thread::sleep_for(std::chrono::milliseconds(1500));
-    client->msg("(server:t).play");
+    client->msg("{SinOsc.ar(mul:Env.perc.kr(2) * 0.3)}.play");
     std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-    client->msg("t.quit");
+    client->msg("s.quit");
     server_thread.join();
     std::this_thread::sleep_for(std::chrono::milliseconds(3000));
     // destroyLanguageClient(client);
