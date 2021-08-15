be6c41672f51336fad0a44f84cccd7fcaac5da78
simon <simoncornaz@gmail.com>
2021-08-15 16:31:56 +0200

    prschedulechanged?

--- test/include.h
+++ test/include.h
@@ -28,24 +28,43 @@ static XenomaiInitializer xenomaiInitializer;
 int synth_main();
 
 #include "SC_LanguageClient.h"
+#include <PyrPrimitive.h>
 #include <iostream>
 
 struct LangClient : SC_LanguageClient {
     FILE* gPostDest = stdout;
 
-    void msg(const char *msg);
-    LangClient(const char *name);
+    LangClient(const char* name);
 
     virtual void postText(const char* str, size_t len) { fwrite(str, sizeof(char), len, gPostDest); }
+
     virtual void postFlush(const char* str, size_t len) {
         fwrite(str, sizeof(char), len, gPostDest);
         fflush(gPostDest);
     }
+
     virtual void postError(const char* str, size_t len) {
         fprintf(gPostDest, "ERROR: ");
         fwrite(str, sizeof(char), len, gPostDest);
     }
+
     virtual void flush() { fflush(gPostDest); }
+
+    void msg(const char* msg);
+
+    static int prScheduleChanged(VMGlobals* g, int numArgsPushed) {
+        return 0;
+    }
+
+    virtual void onLibraryStartup() {
+        int base, index = 0;
+
+        base = nextPrimitiveIndex();
+        // definePrimitive(base, index++, "_Argv", &SC_TerminalClient::prArgv, 1, 0);
+        // definePrimitive(base, index++, "_Exit", &SC_TerminalClient::prExit, 1, 0);
+        definePrimitive(base, index++, "_AppClock_SchedNotify", prScheduleChanged, 1, 0);
+        // definePrimitive(base, index++, "_Recompile", &SC_TerminalClient::prRecompile, 1, 0);
+    }
 };
 
 std::unique_ptr<LangClient> lang_main();
